7ae0d3adaec396e8babb759d2a262b7843357d3a
muhammadkaif-razvi <muhammadkaifrazwi@gmail.com>
2025-07-13 19:58:48 +0530

    feat: Remove next-sitemap dependency and add sitemap generation logic

--- next-sitemap.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('next-sitemap').IConfig} */
-module.exports = {
-  siteUrl: 'https://www.muhammadkaifrazvi.xyz',
-  generateRobotsTxt: true,
-  generateIndexSitemap: false,
-  outDir: './public',
-  sourceDir: 'app', // 👈 this is required for App Router
-};
