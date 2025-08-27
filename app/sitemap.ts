c7210d57c0a9112953e72580f1b290178f778f4b
muhammadkaif-razvi <muhammadkaifrazwi@gmail.com>
2025-08-27 19:32:09 +0530

    fix: Update robots and sitemap configurations for improved crawling and indexing

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -9,6 +9,7 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   const Entries: MetadataRoute.Sitemap = paths.map((path) => ({
     url: `${BaseUrl}${path}`,
     lastModified: new Date(),
+    changeFrequency: "daily",
   }));
   return [
     {
